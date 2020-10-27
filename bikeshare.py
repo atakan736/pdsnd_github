5164af091a81097197692600d7c4786ce5b9ce78
Atakan Celik <atakan.celik@turkcell.com.tr>
2020-10-28 00:24:40 +0300

    Refactoring Bikeshare File to remove import Import numpy as np is unnecesary and slow down running because numpy library is not used in the code

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,6 @@
 import time
 import pandas as pd
-import numpy as np
+
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
@@ -24,7 +24,7 @@ def get_filters():
             break
         else:
             print("invalid input. Please enter a valid input")
-            
+
     # TO DO: get user input for month (all, january, february, ... , june)
     while True:
         month = input("Do you want details specific to a particular month? If yes, type month name from within first six months else type 'all'")
@@ -33,7 +33,7 @@ def get_filters():
             break
         else:
             print("invalid input. Please enter a valid input")
-           
+
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     while True:
         day = input("Do you want details specific to a particular day? If yes, type day name else type 'all'")
@@ -42,7 +42,7 @@ def get_filters():
             break
         else:
             print("invalid input. Please enter a valid input")
-            
+
     print('-'*40)
     return city, month, day
 
@@ -62,21 +62,21 @@ def load_data(city, month, day):
     df["Start Time"] = pd.to_datetime(df["Start Time"])
     df["month"] = df["Start Time"].dt.month
     df["day_of_week"] = df["Start Time"].dt.weekday_name
-    
+
     if month != "all":
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(months)+1
         df = df[df['month'] == month]
-        
+
     if day != "all":
         df = df[df['day_of_week'] == day.title()]
-   
+
     return df
 
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
-    
+
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
@@ -89,9 +89,9 @@ def time_stats(df):
     # TO DO: display the most common start hour
     df["hour"] = df["Start Time"].dt.hour()
     print("The most common start hour is", df["hour"].mode()[0])
-           
+
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40) 
+    print('-'*40)
 
 
 def station_stats(df):
@@ -114,9 +114,9 @@ def station_stats(df):
     print('-'*40)
 
 
-def trip_duration_stats(df): 
+def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
-    
+
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
@@ -138,7 +138,7 @@ def user_stats(df):
 
     # TO DO: Display counts of user types
     user_types = df.groupby(["User Types"])["User Types"].count()
-    print(user_types)      
+    print(user_types)
 
     # TO DO: Display counts of gender
     if city != "washington":
@@ -151,7 +151,7 @@ def user_stats(df):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-          
+
     x = 0
     while True:
         raw = input('\nWould you like to see some raw data? Enter yes or no.\n')
@@ -160,7 +160,7 @@ def user_stats(df):
             x += 5
         else:
             break
-          
+
 
 def main():
     while True:
@@ -171,8 +171,8 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-          
-          
+
+
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
